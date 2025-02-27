21e6d2ef33a6b194729ebf218e887c4549c6c29d
j-wielg <jwielgus@nd.edu>
2025-02-27 08:38:52 -0500

    feat: Added the IP and IP_inverse permutations to the `DES` class

--- des.py
+++ des.py
@@ -71,6 +71,26 @@ class DES:
     iteration_table: list[int] = [
         1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1
     ]
+    IP: list[int] = [
+        58, 50, 42, 34, 26, 18, 10, 2,
+        60, 52, 44, 36, 28, 20, 12, 4,
+        62, 54, 46, 38, 30, 22, 14, 6,
+        64, 56, 48, 40, 32, 24, 16, 8,
+        57, 49, 41, 33, 25, 17, 9,  1,
+        59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5,
+        63, 55, 47, 39, 31, 23, 15, 7
+    ]
+    IP_inv: list[int] = [
+        40, 8, 48, 16, 56, 24, 64, 32,
+        39, 7, 47, 15, 55, 23, 63, 31,
+        38, 6, 46, 14, 54, 22, 62, 30,
+        37, 5, 45, 13, 53, 21, 61, 29,
+        36, 4, 44, 12, 52, 20, 60, 28,
+        35, 3, 43, 11, 51, 19, 59, 27,
+        34, 2, 42, 10, 50, 18, 58, 26,
+        33, 1, 41,  9, 49, 17, 57, 25,
+    ]
     round_keys: list[int]
 
     def __init__(self, _key: int):
@@ -91,5 +111,7 @@ class DES:
             temp = permute(self.PC2, temp, 56)
             self.round_keys.append(temp)
 
-    def encrypt(self):
-        pass
+    def encrypt_block(self, block: int) -> int:
+        '''
+        Encrypts a 64-bit block of data using the DES algorithm
+        '''
